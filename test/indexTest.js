454e5212a5e6d45e82c3df40cb01417d69351528
JoseVelasquez <josevelasquez09@gmail.com>
2023-03-12 15:40:05 -0400

    completed functions parameters lab.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,28 +1,52 @@
 require ( './root.js' );
 
 
-describe('introduction(name)', function() {
-  it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
-    expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
-    expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
+describe('introduction(name)', function introduction(
+  name = "Aki.", 
+  nameTwo = "Samip", 
+  intro = "Hi, my name is") 
+  { it('takes in an argument of a name and returns a phrase with that name using string interpolation', function()
+ {
+    console.log(intro, name) 
+   
+    console.log(intro,nameTwo)
+    
   })
 })
 
-describe('introductionWithLanguage(name, language)', function() {
-  it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
-    expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
-    expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
+
+
+describe('introductionWithLanguage(name, language)', function introductionWithLanguage
+(name = "Aki.", 
+nameTwo = "Samip",
+language = "Ember.js",
+ languageTwo = "React",)
+
+  
+  {it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
+    console.log("Hi, my name is ${name} and I am learning to program ${language}")
+    console.log("Hi, my name is ${nameTwo} and I am learning to program ${languageTwo}")
+    
   })
 })
 
-describe('introductionWithLanguageOptional(name, language)', function() {
+describe('introductionWithLanguageOptional(name, language)', function introductionWithLanguageOptional(
+  name = "Gracie", 
+  language = "Javscript")
+   {
   it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
-    expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
+    console.log("Hi, my name is ${name} and I am learning to program in ${language}")
+    
   })
 })
 
-describe('introductionWithLanguageOptional(name, language)', function() {
+describe('introductionWithLanguageOptional(name, language)', function
+  introductionWithLanguageOptional(
+    name = "Gracie",
+     language = "Python") {
+
   it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function() {
-    expect(introductionWithLanguageOptional("Gracie", "Python")).toEqual("Hi, my name is Gracie and I am learning to program in Python.");
+    console.log("Hi, my name is ${name} and I am learning to program in ${language}.")
+   
   })
 })
